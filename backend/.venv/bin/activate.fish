105dc2632d87967633c717b87f2630539e14007c
Sunish Oturkar <sunish@macbookpro.lan>
2025-09-14 16:52:46 -0400

    Add Railway configuration

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sunish/Desktop/cashflow-dashboard/backend/.venv"
+set -gx VIRTUAL_ENV "/Users/sunish/Desktop/cashflow-dashboard-clean/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
